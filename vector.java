d68c53c7769c30a90af7cb0fa1a2d106fd3042c2
Prisha <153307711+prisha2012@users.noreply.github.com>
2026-03-29 12:00:00 +0530

    93

---
+++ vector.java
@@ -0,0 +1,20 @@
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        Vector<Integer> v = new Vector<>();
+
+        v.add(10);
+        v.add(20);
+        v.add(30);
+
+        System.out.println(v);
+
+        v.remove(1);
+
+        System.out.println(v);
+    }
+}
+public class vector {
+    
+}
